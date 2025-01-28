062859ae22c96616a3380ea5d441f0686dcb35d5
imtaki <161586687+imtaki@users.noreply.github.com>
2025-01-28 10:16:21 +0100

    axios fix

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaUserCircle, FaPaw, FaEnvelope, FaCalendar } from "react-icons/fa";
 import moment from "moment";
-import { api } from "../axios";
+import api from "../axios";
 const Profile: React.FC = () => {
   const navigate = useNavigate();
   const [userData, setUserData] = useState({
